af92bd38adc8debacf954213158996c70bd021a9
JuliusBot <75899898+Julius-Bu@users.noreply.github.com>
2022-05-18 16:44:54 +0300

    edits

--- home.php
+++ home.php
@@ -73,22 +73,56 @@ if(isset($_SESSION['email']) && $credential_row['type']=='admin'){
 <!DOCTYPE html>
 <html>
     <head>
+
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Buwembo Julius">
+    <meta name="generator" content="Hugo 0.84.0">
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="dashboard.css" rel="stylesheet">
+
+
+
       <!-- //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
       // Links to this page start
       //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| -->
 
       <link rel="shortcut icon" href="img/lifestyleStore.png" />
-      <title>HavaTech</title>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Julius 2022</title>
       <!-- latest compiled and minified CSS -->
-      <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
       <link rel="stylesheet" href="fontawesome/css/all.min.css" type="text/css">
       <link rel="stylesheet" href="fontawesome/css/all.css" type="text/css">
       <!-- jquery library -->
       <script type="text/javascript" src="bootstrap/js/jquery-3.2.1.min.js"></script>
       <!-- Latest compiled and minified javascript -->
-      <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+      
       <script type="text/javascript" src="fontawesome/js/all.min.js"></script>
       <script type="text/javascript" src="fontawesome/js/all.js"></script>
       <!-- External CSS -->
@@ -108,29 +142,127 @@ if(isset($_SESSION['email']) && $credential_row['type']=='admin'){
     </head>
 
     <body>
-      <div class="container-fluid">
-           
-           <!-- Background Image start -->
-           <img src="img/bg.jpg" style="z-index: -1; position: fixed; width: 100%; margin-top: 50px;">
+      <!-- Background Image start -->
+      <img src="img/bg.jpg" class="img-responsive" style="z-index: -1; position: fixed; width: 100%; margin-top: 50px;">
            <!-- Background Image end -->
-           <div class="row">
-             <div class="col-md-3" style="background: green;">one
+    
+    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
+  <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+  <div class="navbar-nav">
+    <div class="nav-item text-nowrap">
+      <a class="nav-link px-3" href="#">Sign out</a>
+    </div>
+  </div>
+</header>
 
-             </div>
-             <div class="col-md-9">two
+<div class="container-fluid">
+  <div class="row">
+    <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+      <div class="position-sticky pt-3">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">
+              <span data-feather="home"></span>
+              Dashboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file"></span>
+              Orders
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="shopping-cart"></span>
+              Products
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="users"></span>
+              Customers
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="bar-chart-2"></span>
+              Reports
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="layers"></span>
+              Integrations
+            </a>
+          </li>
+        </ul>
 
-             <?php
-            require 'header.php';
-           ?>
-               <!-- //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Saved reports</span>
+          <a class="link-secondary" href="#" aria-label="Add a new report">
+            <span data-feather="plus-circle"></span>
+          </a>
+        </h6>
+        <ul class="nav flex-column mb-2">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Current month
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Last quarter
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Social engagement
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              <span data-feather="file-text"></span>
+              Year-end sale
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Dashboard</h1>
+        <div class="btn-toolbar mb-2 mb-md-0">
+          <div class="btn-group me-2">
+            <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+          </div>
+          <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+            <span data-feather="calendar"></span>
+            This week
+          </button>
+        </div>
+      </div>
+
+      <canvas class="my-4 w-100" id="myChart" width="900" height="380"></canvas>
+
+        <!-- //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
             // Dashboard Section start
             //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| -->
             
-           <div class="container" id="db">
+            <div class="container" id="db">
             <h1 style="background: #F5F5F5; margin-top: 50px;">My Dashboard</h1>
                <div class="row">
 
-                   <div class="col-xs-4">
+                   <div class="col-xs-4 col-md-4 col-sm-4 col-lg-4">
                        <div  class="thumbnail">
                            <center>
                             <a href="#sr">
@@ -144,7 +276,7 @@ if(isset($_SESSION['email']) && $credential_row['type']=='admin'){
                        </div>
                    </div>
 
-                   <div class="col-xs-4">
+                   <div class="col-xs-4 col-md-4 col-sm-4 col-lg-4">
                        <div  class="thumbnail">
                            <center>
                             <a href="#sr">
@@ -164,7 +296,7 @@ if(isset($_SESSION['email']) && $credential_row['type']=='admin'){
                        </div>
                    </div>
 
-                   <div class="col-xs-4">
+                   <div class="col-xs-4 col-md-4 col-sm-4 col-lg-4">
                        <div  class="thumbnail">
                            <center>
                             <a href="#cr">
@@ -177,8 +309,10 @@ if(isset($_SESSION['email']) && $credential_row['type']=='admin'){
                            </center>
                        </div>
                    </div>
+               </div>
 
-                   <div class="col-xs-4">
+               <div class="row">
+               <div class="col-xs-4 col-md-4 col-sm-4 col-lg-4">
                        <div  class="thumbnail">
                            <center>
                             <a href="#pr">
@@ -198,7 +332,7 @@ if(isset($_SESSION['email']) && $credential_row['type']=='admin'){
                        </div>
                    </div>
 
-                   <div class="col-xs-4">
+                   <div class="col-xs-4 col-md-4 col-sm-4 col-lg-4">
                        <div  class="thumbnail">
                            <center>
                             <a href="uploader.php">
@@ -212,7 +346,7 @@ if(isset($_SESSION['email']) && $credential_row['type']=='admin'){
                        </div>
                    </div>
 
-                   <div class="col-xs-4">
+                   <div class="col-xs-4 col-md-4 col-sm-4 col-lg-4">
                        <div  class="thumbnail">
                            <center>
                             <a href="#db">
@@ -225,7 +359,6 @@ if(isset($_SESSION['email']) && $credential_row['type']=='admin'){
                            </center>
                        </div>
                    </div>
-                   
                </div>
             </div>
 
@@ -233,8 +366,7 @@ if(isset($_SESSION['email']) && $credential_row['type']=='admin'){
                 // Dashboard Section end
                 //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| -->
 
-
-            <!-- //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+                <!-- //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
             // Sales Report Section start
             //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| -->
             <div id="sr" class="container">
@@ -384,13 +516,18 @@ if(isset($_SESSION['email']) && $credential_row['type']=='admin'){
             <!-- //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
             // Customers Report Section end
             //|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| -->
-             </div>
-           </div>
+
+      
+    </main>
+  </div>
+</div>
 
 
-            
-           
+    <script src="bootstrap.bundle.min.js"></script>
 
+      <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="dashboard.js"></script>
+      <div class="container-fluid">
+          
             <br><br> <br><br><br><br>
             <!-- //||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
             // Footer Section start
@@ -398,6 +535,7 @@ if(isset($_SESSION['email']) && $credential_row['type']=='admin'){
            <footer class="footer"> 
                <div class="container">
                <center> 
+                 &copy Buwembo Julius 2022
                </center>
                </div>
            </footer>
